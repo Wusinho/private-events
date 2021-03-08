e6b218bc68e90621397b146ec7e2e7aec601c546
Heber <heberlazo@gmail.com>
2021-03-08 14:28:00 -0500

    Add registration controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    def index
+        if session[:user_id]
+            @user =User.find(session[:user_id])
+        end
+        
+    end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,6 +8,7 @@ class RegistrationsController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save 
+            session[:user_id] = @user.id
             redirect_to root_path, notice: "Succesfully created account"
         else
             render :new
