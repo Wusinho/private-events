69c9557620f6d9021a24b1c0aac37f129d984471
Heber <heberlazo@gmail.com>
2021-03-08 14:28:40 -0500

    Add main controller and user cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-    def index
-        if session[:user_id]
-            @user =User.find(session[:user_id])
-        end
-        
-    end
 end
