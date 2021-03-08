8ffecfe220e0ac805cdb2362c20d023415bf436d
Heber <heberlazo@gmail.com>
2021-03-08 12:42:31 -0500

    Add users validations and redo

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_03_08_173709) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "username", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
