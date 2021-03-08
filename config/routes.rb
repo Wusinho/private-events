9d31edf424f121eb83f206eadd8b23f2757664e9
Heber <heberlazo@gmail.com>
2021-03-08 15:47:10 -0500

    Add user sign out

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 get "sign_up", to: "registrations#new"
 post "sign_up", to: "registrations#create"
 
+get "sign_in", to: "sessions#new"
+post "sign_in", to: "sessions#create"
+
 delete "logout", to: "sessions#destroy"
 
 root "events#index"
