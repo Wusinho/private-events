ba548b061f48396fa1b6e31b62cfe2facfafb462
Heber <heberlazo@gmail.com>
2021-03-15 10:59:37 -0500

    Stuck on double user?

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,26 +12,26 @@ class EventsController < ApplicationController
       @attendance = Attendance.new
       @users_not_in = User.all.where.not(id: @event.attendee).where.not(id: session[:current_user]['id'])
     else
-      redirect_to root_path
+      redirect_to sign_in_path
     end
   end
 
   def create
     @user = User.find_by(username: params[:username])
-    @event = user.events.build(event_params)
+    @event = @user.events.build(event_params)
 
     if @event.save
-      redirect_to root_path
+      redirect_to events_path
     else
       render :new
     end
   end
 
   def new
-    if session[:current_user]
+    if Current.user
       @event = Event.new
     else
-      redirect_to root_path
+      redirect_to sign_in_path
     end
   end
 
