7dbf5ff2958829e4076cb4f081abe6662b185dce
Heber <heberlazo@gmail.com>
2021-03-16 13:51:53 -0500

    Fix bug with attendacen

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,40 +1,43 @@
 class EventsController < ApplicationController
- 
+  before_action :set_event, only: %i[show edit update destroy]
+
+  # GET /events or /events.json
   def index
-    @upcoming = Event.upcoming.sort { |a, b| a.date <=> b.date }
-    @past = Event.past.sort { |a, b| b.date <=> a.date }
+    @events = Event.all
   end
 
-  def show
-    if session[:user_id]
-      @event = Event.find_by(params[:id])
-      @date = date_now
-      @attendance = Attendance.new
-      @users_not_in = User.all.where.not(id: @event.attendee).where.not(id: Current.user.id)
+  # GET /events/1 or /events/1.json
+  def show; end
+
+  # GET /events/new
+  def new
+    if set_current_user.nil?
+      flash.alert = 'You have to login first'
+      redirect_to root_path
     else
-      redirect_to sign_in_path
+      @event = Event.new
     end
   end
 
-  def create
-    #@user = User.find_by(username: params[:username])
-    @event = Current.user.events.build(event_params)
-
-    if @event.save
-      redirect_to events_path
-    else
-      render :new
-    end
-  end
-
-  def destroy
-    @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # GET /events/1/edit
   def edit; end
+
+  # POST /events or /events.json
+  def create
+    @event = Event.new(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /events/1 or /events/1.json
   def update
     respond_to do |format|
       if @event.update(event_params)
@@ -47,21 +50,25 @@ class EventsController < ApplicationController
     end
   end
 
-  def new
-    if Current.user
-      @event = Event.new
-    else
-      redirect_to root_path
+  # DELETE /events/1 or /events/1.json
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
-  def enroll
-    @user = User.find_by(username: params[:username])
-  end
-
 
   private
-  def event_params
-    params.require(:event).permit(:title, :date, :location, :body, :user_id)
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_event
+    @event = Event.find(params[:id])
   end
 
+  # Only allow a list of trusted parameters through.
+  def event_params
+    # params.fetch(:event, {})
+    params.require(:event).permit(:title, :body, :date, :location).merge(user_id: set_current_user.id)
+  end
 end
