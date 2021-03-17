c8aeaca836fc08bdb9444fe8a2a44aeb83adc4ee
Heber <heberlazo@gmail.com>
2021-03-17 12:54:56 -0500

    Remove conditions from views

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,9 +1,10 @@
-module EventsHelper
+module EventsHelper 
+  include UsersHelper
   def navbar
     if Current.user
       content_tag(:span, Current.user.username, class: %w[navbar-text mr-2]) +
         link_to('New Event', new_event_path, class: 'btn btn-secondary') +
-        link_to('My Events', user_path(Current.user.id), class: 'btn btn-secondary') +
+        eventsany +
         link_to('Log Out', sign_out_path, data: { confirm: 'Are you sure?' }, class: 'btn btn-secondary')
     else
       link_to('Sign In', sign_in_path, class: 'nav-link active') +
@@ -43,4 +44,7 @@ module EventsHelper
   def editevent
     link_to('Edit', edit_event_path(@event), class: 'btn btn-warning') if set_current_user && set_current_user.id == @event.user_id
   end
+
+ 
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,27 @@
 module UsersHelper
+
+
+  def eventsany
+
+      link_to('My Events', user_path(Current.user.id), class: 'btn btn-secondary') 
+  end
+    
+
+  def userl(event)
+
+    if DateTime.now.utc <= event.date
+      content_tag(:td,  event.title ) +
+      content_tag(:td, event.date ) 
+    end
+  end
+  
+
+  def userg(event)
+
+    if DateTime.now.utc > event.date
+      content_tag(:td,  event.title ) +
+      content_tag(:td, event.date ) 
+    end
+  end
+
 end
